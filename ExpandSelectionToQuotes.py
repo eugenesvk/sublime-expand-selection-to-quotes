863755c13d01df68595b74014d8085e632d8ec85
eugenesvk <eugenesvk@users.noreply.github.com>
2026-04-05 14:36:12 +0700

    add proper example syntax rules for escapes
    if quote can act as an escape, need to consider or following symbols to determine whether this is a standalone quote (include) or an escape char (ignore)

--- ExpandSelectionToQuotes.py
+++ ExpandSelectionToQuotes.py
@@ -2,9 +2,13 @@ import sublime, sublime_plugin
 
 class ExpandSelectionToQuotesCommand(sublime_plugin.TextCommand):
 	q_same = ('"', "'", "`")
-	esc = {'':'\\', # TODO: add more non\ escaping languages (custom symbol and quote repeats)
-		'source.ahk':'`',
-		'source.python':'\\',
+	q_paired = (("«","»"), ("‹","›"), ("‘","’"), ("‛","’"), ("“","”"), ("‟","”"), ("„","“"), ("🙶","🙷"))
+	esc = {'':{'c':'\\', 'sym':['"',"'"]},
+		# since escape char is a quote ↓ incluce non-quotes to exclude ` from matches
+		'source.ahk'  : {'c':'`' , 'sym':['"',"'","`", ";",":","{","n","r","b","t","s","v","a","f",]},
+		'source.ahk.1': {'c':'`' , 'sym':['"',"'","`", ";",":","{","n","r","b","t","s","v","a","f",]},
+		'source.ahk.2': {'c':'`' , 'sym':['"',"'","`", ";",":","{","n","r","b","t","s","v","a","f",]},
+		'source.python':{'c':'\\', 'sym':['"',"'"]}, #ignore non quotes: \n \ a b f n N{name} r t uxxxx Uxxxxxxxx v ooo xhh
 	}
 	esc_self = {'':('',), # languages that allow repeated quotes to escape themselves
 		'source.ahk.1':('"',"'"),
@@ -21,31 +25,42 @@ class ExpandSelectionToQuotesCommand(sublime_plugin.TextCommand):
 		for q in self.q_same:
 			q_all[q] = list(map(lambda x: x.begin(), self.view.find_all(q)))
 
-		def search_for_quotes(q_type, quotes, esc, esc_self):
+		def search_for_quotes(q, quotes, esc, esc_self):
 			q_size, before, after = False, False, False
 
 			if len(quotes) - self.view.substr(sel).count('"') >= 2:
 				all_before = list(filter(lambda x: x <  sel.begin(), quotes))
 				all_after  = list(filter(lambda x: x >= sel.end  (), quotes))
 				before, after = None, None
+				esc_c = esc['c'  ] # {c: \    sym: " '}
+				is_esc = True if q == esc_c else False # single quote can act as an escape
 
 				if all_before: # Find escaped quotes and skip them
 					for  i_q in reversed(all_before):
-						if i_q == 0:
+						if is_esc: # check if this is an escape for the next char `', not a standalone quote `
+							char_pos_q = None if (i_q + 1) >= self.view.__len__() else self.view.substr(i_q + 1)
+							if char_pos_q and char_pos_q in esc['sym']: # not a quote, but an escape
+								continue
+						if i_q == 0: # ␂, so don't check for escape char before this
 							before = i_q
 							break
 						else:
 							char_pre_q = self.view.substr(i_q - 1)
-							if   char_pre_q == esc:
-								pass
+							if   char_pre_q == esc_c:
+								continue
 							elif char_pre_q == q and q in esc_self:
-								pass
+								continue
 							else:
 								before = i_q
 								break
 				if all_after:
 					skip_paired = False
 					for  i_q in          all_after  :
+						if is_esc: # check if this is an escape for the next char `', not a standalone quote `
+							char_pos_q = None if (i_q + 1) >= self.view.__len__() else self.view.substr(i_q + 1)
+							if char_pos_q and char_pos_q in esc['sym']: # not a quote, but an escape
+								continue
+
 						if skip_paired:
 							skip_paired = False
 							continue
@@ -56,8 +71,7 @@ class ExpandSelectionToQuotesCommand(sublime_plugin.TextCommand):
 						else:
 							char_pre_q = self.view.substr(i_q - 1)
 							char_pos_q = None if (i_q+1) >= self.view.__len__() else self.view.substr(i_q + 1)
-							# print(f"{i_q} ? {self.view.__len__()} char_pos_q={char_pos_q}")
-							if   char_pre_q == esc:
+							if   char_pre_q == esc_c:
 								pass
 							elif char_pos_q == q and q in esc_self: # double quote as escape
 								skip_paired = True
@@ -77,10 +91,14 @@ class ExpandSelectionToQuotesCommand(sublime_plugin.TextCommand):
 			self.view.sel().add(sublime.Region(start, end))
 
 		for sel in self.view.sel():
-			ctx = self.view.scope_name(sel.a) #e.g., source.python meta.function…
-			ctx_source = ctx.split()[0]       #      source.python
-			esc      = self.esc     .get(ctx_source, self.esc     [''])
-			esc_self = self.esc_self.get(ctx_source, self.esc_self[''])
+			ctx = self.view.scope_name(sel.a) #e.g., "source.python meta.function…"
+			ctx_source = ctx.split()          #      [source.python,meta.function,…]
+			esc, esc_self = None, None
+			for  ctx_i in ctx_source:         #       source.python
+				if esc      is None and ctx_i in self.esc     : esc      = self.esc     .get(ctx_i)
+				if esc_self is None and ctx_i in self.esc_self: esc_self = self.esc_self.get(ctx_i)
+			if esc      is None: esc      = self.esc     ['']
+			if esc_self is None: esc_self = self.esc_self['']
 
 			q_res = {}
 			for q in self.q_same:
