466d95b50ef9f34ce2098abb57d50062b1373dc4
eugenesvk <eugenesvk@users.noreply.github.com>
2026-04-07 17:17:31 +0700

    🧪string: add ‘pair-different’ quotes

--- test/string.py
+++ test/string.py
@@ -8,3 +8,7 @@ c = '•Single quoted selected, \"\' esc⎀ped \'\" quotes ignored •'
   #               meta.string ↑↑↑↑ string.quoted.single  constant.character.escape
   #               meta.string ↓↓↓↓ string.quoted.double  constant.character.escape
 d = "•Double quoted selected, \"\' esc⎀ped \'\" quotes ignored •"
+
+e = '•Ignore shorter pair of →" ⎀•'  +  '"← because this is a different string'
+f = '•Ignore shorter pair of →‟ ⎀•'  +  '”← because this is a different string'
+g = ' Select …       …       →‟•⎀       •”←               the same      …     '
