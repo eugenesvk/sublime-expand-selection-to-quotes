5e34af5c6fab7fa4eefa48ca01e41c694551fe9f
eugenesvk <eugenesvk@users.noreply.github.com>
2026-04-07 15:36:37 +0700

    🧪string: update

--- test/string.py
+++ test/string.py
@@ -3,3 +3,8 @@ a = 'test' + "←this ⎀ shouldn't break _____________________________________ 
 #vs          ←                                                              →
 #but         "" string scope limits search scope
 b = 'test' + "    '←⎀ txt→'"
+
+c = '←Single quoted selected, \"\' esc⎀ped \'\" quotes ignored →'
+  #               meta.string ↑↑↑↑ string.quoted.single  constant.character.escape
+  #               meta.string ↓↓↓↓ string.quoted.double  constant.character.escape
+d = "←Double quoted selected, \"\' esc⎀ped \'\" quotes ignored →"
