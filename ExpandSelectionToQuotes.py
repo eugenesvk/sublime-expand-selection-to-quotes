cfbfcbfda3e41234e303a798477b19f7a80d4112
eugenesvk <eugenesvk@users.noreply.github.com>
2026-04-07 18:45:44 +0700

    support paired quotes and also including quotes in selection

--- ExpandSelectionToQuotes.py
+++ ExpandSelectionToQuotes.py
@@ -9,7 +9,7 @@ _log.setLevel(DEFAULT_LOG_LEVEL)
 _L = False #dbg
 
 class ExpandSelectionToQuotesCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
+  def run(self, edit, qp=False, inc=False):
     C = cfg.cfgU.C
     view = self.view
 
@@ -17,10 +17,16 @@ class ExpandSelectionToQuotesCommand(sublime_plugin.TextCommand):
     for q in C['q=']: # " ' `
       q_pt_all[q] = list(map(lambda x: x.begin(), view.find_all(q)))
 
-    def search_for_quotes(q, q_pts, txt_pt):
-      q_size, before, after = False, False, False
-      if len(q_pts) - view.substr(sel).count('"') < 2: # not enough pairs
-        return q_size, before, after
+    def search_for_quotes(q, q_pts, txt_pt, is_p=False):
+      q_size, before,after = False, False, False
+      ql_pre = len(q[0]) if is_p else 1
+      ql_pos = len(q[1]) if is_p else 1
+      q_pts_beg = q_pts['beg'] if is_p else q_pts
+      q_pts_end = q_pts['end'] if is_p else q_pts
+      count_q = len(q_pts_beg) + (len(q_pts_end) if is_p else 0)
+      count_qq = view.substr(sel).count('"')
+      if count_q - view.substr(sel).count('"') < 2: # not enough pairs
+        return q_size, before,after,  ql_pre,ql_pos
 
       # 1. Limit quotes to a string
       txt_scope = view.scope_name(txt_pt) #e.g., "source.python meta.function…"
@@ -37,12 +43,12 @@ class ExpandSelectionToQuotesCommand(sublime_plugin.TextCommand):
       if str_scope:
         if (s := view.expand_to_scope(txt_pt, str_scope)):
           all_before = list(filter(lambda x: (x <  sel.begin()) \
-            and                              (x >=   s.begin()), q_pts))
+            and                              (x >=   s.begin()), q_pts_beg))
           all_after  = list(filter(lambda x: (x >= sel.end  ()) \
-            and                              (x <=   s.end  ()), q_pts))
+            and                              (x <=   s.end  ()), q_pts_end))
       else:
-        all_before   = list(filter(lambda x:  x <  sel.begin() , q_pts))
-        all_after    = list(filter(lambda x:  x >= sel.end  () , q_pts))
+        all_before   = list(filter(lambda x:  x <  sel.begin() , q_pts_beg))
+        all_after    = list(filter(lambda x:  x >= sel.end  () , q_pts_end))
 
       # 2. Find quotes that do/are not escape(s)
       esc   = C['esc'] # constant.character.escape
@@ -75,7 +81,7 @@ class ExpandSelectionToQuotesCommand(sublime_plugin.TextCommand):
           break
       if before is not None and after is not None: q_size = after - before
 
-      return q_size, before, after
+      return q_size, before,after,  ql_pre,ql_pos
 
     def replace_region(start, end):
       if sel.size() < end-start-2:
@@ -88,8 +94,8 @@ class ExpandSelectionToQuotesCommand(sublime_plugin.TextCommand):
 
       q_res = {}
       for q in C['q=']: # " ' `             ↓list[sublime.Point]
-        sz, pre, pos = search_for_quotes(q, q_pt_all[q], txt_pt)
-        q_res[sz] = (pre,pos)
+        sz, pre,pos,  ql_pre,ql_pos = search_for_quotes(q, q_pt_all[q], txt_pt, is_p=False)
+        q_res[sz] = (pre,pos,  ql_pre,ql_pos)
         if _L: _log.debug(f"q={q} pre={pre} pos={pos} q_pt={q_pt_all[q]}")
 
       min_sz = None
@@ -100,5 +106,9 @@ class ExpandSelectionToQuotesCommand(sublime_plugin.TextCommand):
           else:
             min_sz = min(sz, min_sz)
       if min_sz:      # …use their region for selection
-        replace_region(q_res[min_sz][0], q_res[min_sz][1] + 1)
+        pre_t , pos_t  = q_res[min_sz][0], q_res[min_sz][1]
+        pre_ql, pos_ql = q_res[min_sz][2], q_res[min_sz][3]
+        beg = pre_t     - (pre_ql if inc else 0)
+        end = pos_t + 1 + (pos_ql if inc else 0)
+        replace_region(beg, end)
       if _L: _log.debug(f"min_sz={min_sz}")
