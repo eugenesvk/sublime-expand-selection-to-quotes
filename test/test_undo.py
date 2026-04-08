4d038b16de1f2dbff2d6cd01226bc0d96c15f622
eugenesvk <eugenesvk@users.noreply.github.com>
2026-04-09 00:55:07 +0700

    🧪fix undo by using deferrable test working with async

--- test/test_undo.py
+++ test/test_undo.py
@@ -1,7 +1,7 @@
 # Install `UnitTesting`
 # Run `UnitTesting: Test Current File`/`Package`
 import os, re
-from unittest import TestCase
+from unittesting import DeferrableViewTestCase
 
 import sublime
 
@@ -12,9 +12,12 @@ cfgU_settings = (f'{PACKAGE_NAME}.sublime-settings')
 
 version = sublime.version()
 
-class TestQuotePaired(TestCase):
+class TestUndo(DeferrableViewTestCase):
   def setUp(self):
-    self.view = sublime.active_window().new_file(syntax='Python.sublime-syntax')
+    # self.view = sublime.active_window().new_file(syntax='Python.sublime-syntax') # Flashes with async tests
+    (self.view,self.vin) = sublime.active_window().create_io_panel(name=PACKAGE_NAME, on_input=None, unlisted=True)
+    self.view.assign_syntax('Python.sublime-syntax')
+    self.view.set_scratch(True)
     cfg = sublime.load_settings("Preferences.sublime-settings")
     cfg.set("close_windows_when_empty", False)
     cfgU = sublime.load_settings(cfgU_settings)
@@ -27,7 +30,8 @@ class TestQuotePaired(TestCase):
   def tearDown(self):
     if (view := self.view):
       view.set_scratch(True)
-      view.window().run_command("close_file")
+      # view.window().run_command("close_file")
+      sublime.active_window().destroy_output_panel(name=PACKAGE_NAME)
 
   def setText(self, string):
     self.view.run_command("select_all")
@@ -59,24 +63,19 @@ class TestQuotePaired(TestCase):
         lb = len(set_i['qb'])
         le = len(set_i['qe'])
         for pos_i in pos_valid:
-          # ↓fails to do anything, the atomic command isn't called
-          # view  .run_command("expand_selection_to_quotes",{"qp":set_i['qp'],"inc":inc})
-
-          # ↓fails to do anything
-          # sublime.set_timeout(lambda:
-          #   view.run_command("expand_selection_to_quotes_atomic",{"qp":set_i['qp'],"inc":inc}) ,5)
-
-          # ↓fails to generate granular undos
-          view  .run_command("expand_selection_to_quotes_atomic",{"qp":set_i['qp'],"inc":inc})
+          view.run_command("expand_selection_to_quotes",{"qp":set_i['qp'],"inc":inc})
+          yield #100
           m_i = [m.start() for m in re.finditer(pos_i, pos)]
           beg   =  m_i[0]        ; end   = (beg if len(m_i) == 1 else m_i[1]) + 1
           beg_s = sels[0].begin(); end_s = sels[0].end()
           print(f"{pos_i} → {m_i}  i ≟ s: beg {beg}{'=' if beg == beg_s else '≠'}{beg_s} ¦ end {end}{'=' if end == end_s else '≠'}{end_s}")
           self.assertEqual(beg_s, beg); self.assertEqual(end_s, end)
-        # for pos_i in reversed(pos_valid[:-1]):
-          # view.run_command("undo")
-          # m_i = [m.start() for m in re.finditer(pos_i, pos)]
-          # beg   =  m_i[0]        ; end   = (beg if len(m_i) == 1 else m_i[1]) + 1
-          # beg_s = sels[0].begin(); end_s = sels[0].end()
-          # print(f"UNDO  {pos_i} → {m_i}  i ≟ s: beg {beg}{'=' if beg == beg_s else '≠'}{beg_s} ¦ end {end}{'=' if end == end_s else '≠'}{end_s}")
-          # self.assertEqual(beg_s, beg); self.assertEqual(end_s, end)
+        print(f"   UNDO")
+        for pos_i in reversed(pos_valid[:-1]):
+          view.run_command("soft_undo")
+          yield #100
+          m_i = [m.start() for m in re.finditer(pos_i, pos)]
+          beg   =  m_i[0]        ; end   = (beg if len(m_i) == 1 else m_i[1]) + 1
+          beg_s = sels[0].begin(); end_s = sels[0].end()
+          print(f"{pos_i} → {m_i}  i ≟ s: beg {beg}{'=' if beg == beg_s else '≠'}{beg_s} ¦ end {end}{'=' if end == end_s else '≠'}{end_s}")
+          self.assertEqual(beg_s, beg); self.assertEqual(end_s, end)
